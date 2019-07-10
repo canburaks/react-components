4c103ca0760535b360c4cb4a931ba4371f5fdbb7
Can Burak S <canburak@msn.com>
2019-07-10 19:31:43 +0300

    "."

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import prepack from 'rollup-plugin-prepack';
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
