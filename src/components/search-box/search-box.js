668ceb820e12409d281a8b1420babf102d7160d1
Can Burak S <canburak@msn.com>
2019-07-13 17:28:06 +0300

    navbar arrangement

--- src/components/search-box/search-box.js
+++ src/components/search-box/search-box.js
@@ -162,6 +162,12 @@ export const SearchBox = (props) => {
                 onChange={e => inputHandler(e)}
                 onKeyDown={e => onKeyDown(e)}
             />
+            <svg title={"Get results!"} 
+                stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className={"input-icon"}
+                onClick={e => submitHandler(e)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+                >
+                    <circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+
             <IconSearch 
                 size={24} 
                 strokeWidth={3} className="input-icon" 
