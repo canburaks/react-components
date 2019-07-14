fb3780aa4452d30a99018bcfe270f4963e91f08a
Can Burak S <canburak@msn.com>
2019-07-14 13:05:45 +0300

    youtube player list item arrange

--- cra/src/App.js
+++ cra/src/App.js
@@ -13,8 +13,37 @@ const App = (props) => {
   }
 
   return (
-    <div style={{ minHeight: "200vh", backgroundColor: "grey", display: "block" }}>
-      <NavBar fixed>
+    <div style={{ minHeight: "200vh", backgroundColor: "grey", display: "block", padding:"10vw" }}>
+        <YoutubePlayer videos={videos} />
+      </div>
+  )
+}
+
+export default App
+
+
+function query(input) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const queryResults = movies.filter(movie => movie.name.toLowerCase().includes(input));
+      //console.log("result returned", result)
+      resolve(queryResults)
+    }, 300)
+  });
+}
+
+var movies = [
+  { name: "Space Odyssey", id: 924, poster: "2001.jpg" },
+  { name: "Amarcord", id: 7089, poster: "https://cbs-static.s3.amazonaws.com/static/media/posters/7089/7089-tmdb.jpg" },
+  { name: "Amarcord", id: 7089, poster: "https://cbs-static.s3.amazonaws.com/static/media/posters/7089/7089-tmdb.jpg" }
+
+]
+
+/*An array containing all the country names in the world:*/
+var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia"];
+
+/*
+     <NavBar fixed>
         <NavLink brand>
           <a href="/home/">Brand Name</a>
         </NavLink>
@@ -56,50 +85,6 @@ const App = (props) => {
           animate
         />
         </div>
-      </div>
-  )
-}
-
-export default App
-
-
-function query(input) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      const queryResults = movies.filter(movie => movie.name.toLowerCase().includes(input));
-      //console.log("result returned", result)
-      resolve(queryResults)
-    }, 300)
-  });
-}
-
-var movies = [
-  { name: "Space Odyssey", id: 924, poster: "2001.jpg" },
-  { name: "Amarcord", id: 7089, poster: "https://cbs-static.s3.amazonaws.com/static/media/posters/7089/7089-tmdb.jpg" },
-  { name: "Amarcord", id: 7089, poster: "https://cbs-static.s3.amazonaws.com/static/media/posters/7089/7089-tmdb.jpg" }
-
-]
-
-/*An array containing all the country names in the world:*/
-var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia"];
-
-/*
-      <NavBar fixed>
-        <NavLink brand>
-          <a href="/home/">Brand Name</a>
-        </NavLink>
-
-        <NavLink >
-          <a href="/link1">link1</a>
-        </NavLink>
-
-        <NavLink right>
-          <a href="/right-link1">right-link1</a>
-        </NavLink>
-        <NavLink right onClick={() => alert("asd")}>
-          <button href="/right-button">right-link2</button>
-        </NavLink>
-      </NavBar>
 <div style={{marginTop:50}}>
 	<SearchBox
 		class="static"
